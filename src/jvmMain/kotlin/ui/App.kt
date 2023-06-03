636e1fa18cd4d33d8264b800014b687ae8408920
Juanjo Marti <juanjo.marti@colba.es>
2023-06-03 12:54:54 +0200

    Interfaz de Cliente

--- src/jvmMain/kotlin/ui/App.kt
+++ src/jvmMain/kotlin/ui/App.kt
@@ -1,10 +1,24 @@
 package ui
 
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import ui.screns.detail.Detail
 import ui.screns.home.Home
+
+sealed interface Route {
+    object Home : Route
+    data class Detail(val id: Long) : Route
+}
 
 @Composable
 fun App() {
-    Home()
+
+    var route by remember { mutableStateOf<Route>(Route.Home) }
+
+    route.let {
+        when (it) {
+            Route.Home -> Home(onCreateClick = { route = Route.Detail(-1) })
+            is Route.Detail -> Detail(it.id, onClose = { route = Route.Home })
+        }
+    }
 }
 
